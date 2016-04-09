181015bb8f8a5426e61fbcab1f557d72f28668c7
hjoanne <jh.joanne.huang@gmail.com>
2016-04-09 14:22:20 -0400

    files added

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.listen(app.get('port'), function() {
 });
 
 var mongoose = require('mongoose');
-mongoose.connect('process.env.MONGOLAB_URI');
+mongoose.connect(process.env.MONGOLAB_URI);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
